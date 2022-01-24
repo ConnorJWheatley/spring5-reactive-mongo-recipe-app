fd1a3ef49a931758c899d884b0809a6c463bf166
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-24 17:25:58 +0000

    Refactored recipe service. Lead to refactoring a lot of other classes and tests

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -3,7 +3,6 @@ package guru.springframework.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashSet;
@@ -19,6 +18,7 @@ public class Recipe {
 
     @Id
     private String id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
